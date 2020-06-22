97562dea454fbe4a04dc17b955b4c5831aa578e2
dwaro <davidjwaro@gmail.com>
2020-06-22 12:04:36 -0500

    description setup

--- client/src/components/description/Description.js
+++ client/src/components/description/Description.js
@@ -20,21 +20,11 @@ const getContent = (algo) => {
   );
 };
 
-const getImage = (algo) => {
-  return <div>Image placeholder</div>;
-};
-
-const getCode = (algo) => {
-  return <div>Code placeholder</div>;
-};
-
 const Description = (props) => {
   return (
     <div>
       <h1>{props.algorithm}</h1>
       {getContent(props.algorithm)}
-      {getImage(props.algorithm)}
-      {getCode(props.algorithm)}
     </div>
   );
 };
