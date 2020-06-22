ff39402b1d7d4acf1ab24f8493e0b79ef05abc68
dwaro <davidjwaro@gmail.com>
2020-06-22 13:54:20 -0500

    change code to inherit scroll

--- client/src/components/description/Description.js
+++ client/src/components/description/Description.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import data from '../../data/sortingData.js';
-import theme from 'prism-react-renderer/themes/nightOwl';
-import styled from 'styled-components';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-
-const Pre = styled.pre`
-  text-align: left;
-  padding: 0.5em;
-  overflow-x: scroll;
-`;
-
-const Line = styled.div`
-  display: table-row;
-`;
-
-const LineNo = styled.span`
-  display: table-cell;
-  text-align: right;
-  padding-right: 1em;
-  user-select: none;
-  opacity: 0.5;
-`;
-
-const LineContent = styled.span`
-  display: table-cell;
-`;
+import CodeSample from '../codeSample/CodeSample';
 
 const getContent = (algo) => {
   let keyCounter = 0;
@@ -39,26 +15,7 @@ const getContent = (algo) => {
         return <p key={keyCounter++}>{p}</p>;
       })}
       <div className="codeSample">Code Sample</div>
-      <Highlight
-        {...defaultProps}
-        theme={theme}
-        code={data[algo].code.trim()}
-        language="java">
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <Pre className={className} style={style}>
-            {tokens.map((line, i) => (
-              <Line key={i} {...getLineProps({ line, key: i })}>
-                <LineNo>{i + 1}</LineNo>
-                <LineContent>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </LineContent>
-              </Line>
-            ))}
-          </Pre>
-        )}
-      </Highlight>
+      <CodeSample data={data[algo].code} language="java" />
     </div>
   );
 };
