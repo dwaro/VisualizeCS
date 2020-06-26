214dfcb96f32e042ff5934c9a4dbba71e54f7b6a
dwaro <davidjwaro@gmail.com>
2020-06-26 15:30:39 -0500

    test bubble sort data output

--- src/__test__/algorithmTests/bubblesort.spec.js
+++ src/__test__/algorithmTests/bubblesort.spec.js
@@ -6,4 +6,54 @@ describe('Bubble Sort algorithm returns correct sorted data', () => {
     const result = bubbleSort(data);
     expect(result.data).toEqual(data);
   });
+
+  it('Odd length input', () => {
+    const data = [1];
+    const result = bubbleSort(data);
+    expect(result.data).toEqual([1]);
+  });
+
+  it('Even length input', () => {
+    const data = [2, 1];
+    const result = bubbleSort(data);
+    expect(result.data).toEqual([1, 2]);
+  });
+
+  it('Effectively medium input -- 1', () => {
+    const data = buildData(999);
+    const result = bubbleSort(data);
+    data.sort();
+    expect(result.data).toEqual(data);
+  });
+
+  it('Effectively medium input -- 2', () => {
+    const data = buildData(1001);
+    const result = bubbleSort(data);
+    data.sort();
+    expect(result.data).toEqual(data);
+  });
+
+  // caps somewhere between 5,000 - 10,000 array size
+  it('Effectively large input -- 1', () => {
+    const data = buildData(4999);
+    const result = bubbleSort(data);
+    data.sort();
+    expect(result.data).toEqual(data);
+  });
+
+  // caps somewhere between 5,000 - 10,000 array size
+  it('Effectively large input -- 2', () => {
+    const data = buildData(5001);
+    const result = bubbleSort(data);
+    data.sort();
+    expect(result.data).toEqual(data);
+  });
 });
+
+const buildData = (size) => {
+  let data = [];
+  for (let i = 0; i < size; i++) {
+    data.push(Math.floor(Math.random() * 1000));
+  }
+  return data;
+};
