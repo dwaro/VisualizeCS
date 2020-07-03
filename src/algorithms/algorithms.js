964009a125e9fbe58c406a32b93e62503e423af5
dwaro <davidjwaro@gmail.com>
2020-07-03 00:57:14 -0500

    add selection sort algorithm to backend

--- src/algorithms/algorithms.js
+++ src/algorithms/algorithms.js
@@ -2,14 +2,17 @@
 //   [[swaps],[current_data_state]]
 // ];
 
+function setup(data) {
+  return [data.slice(), [], 0];
+}
+
 /**
- * This method implements bubble sort to sort its input data.
- * @param {*} data
+ * This function implements bubble sort to sort its input data.
+ *
+ * @param {Array} data - the user's data to be sorted.
  */
 exports.bubbleSort = function bubbleSort(data) {
-  const raw = data.slice();
-  let steps = [];
-  let numSwaps = 0;
+  let [raw, steps, numSwaps] = setup(data);
 
   for (let i = 0; i < data.length - 1; i++) {
     let swaps = [];
@@ -24,6 +27,54 @@ exports.bubbleSort = function bubbleSort(data) {
         data[j + 1] = tmp;
       }
     }
+    const tmpData = data.slice();
+    const step = [swaps, tmpData];
+    steps.push(step);
+  }
+
+  return { data, steps, raw, numSwaps };
+};
+
+/**
+ * This function implements selection sort to sort its input data.
+ *
+ * @param {Array} data - The user's data to be sorted.
+ */
+exports.selectionSort = function selectionSort(data) {
+  let [raw, steps, numSwaps] = setup(data);
+
+  // n - 1 sweeps
+  for (let i = data.length - 1; i > 0; i--) {
+    let swaps = [];
+
+    // index of max element in unsorted portion of the array
+    let max = 0;
+
+    // compare if we have a new max
+    for (let j = 0; j <= i; j++) {
+      if (data[j] > data[max]) {
+        max = j;
+      }
+    }
+
+    // if max is already in position, note that
+    if (max === i) {
+      swaps.push(
+        `The max element, ${data[max]}, at index [${max}], is already in the correct position. No swap necessary.`
+      );
+    } else {
+      swaps.push(
+        `Swap index [${max}], ${data[max]}, and index [${i}], ${data[i]}`
+      );
+
+      numSwaps++;
+
+      // swap max element to the end of unsorted array
+      let tmp = data[i];
+      data[i] = data[max];
+      data[max] = tmp;
+    }
+
     const tmpData = data.slice();
     const step = [swaps, tmpData];
     steps.push(step);
--- src/controllers/sortController.js
+++ src/controllers/sortController.js
@@ -4,6 +4,8 @@ exports.delegate = function delegate(algorithm, data) {
   switch (algorithm) {
     case 'Bubble Sort':
       return algorithms.bubbleSort(data);
+    case 'Selection Sort':
+      return algorithms.selectionSort(data);
     default:
       return '';
   }
