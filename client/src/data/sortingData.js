ee4ed60ac30d167cd795b16a048fda98c3c544a1
dwaro <davidjwaro@gmail.com>
2020-07-20 10:51:41 -0500

    add Java code for Counting sort

--- client/src/data/sortingData.js
+++ client/src/data/sortingData.js
@@ -194,7 +194,60 @@ public static void radixSort(int[] values, int passes) {
       ],
     },
     code: {
-      java: 'Java code for Counting Sort to come!',
+      java: `public static void countingSort(int[] data) {
+  // get the max value in the data, so we know how large to make the counting array
+  int max = getMax(data);
+  
+  int[] counting = new int[max + 1];
+  
+  // build counting array
+  for (int val: data) {
+    counting[val]++;
+  }
+  
+  // sort data
+  int pos = 0;
+  for (int i = 0; i < data.length; i++) {
+    while (pos < counting.length && counting[pos] == 0) {
+      pos++;
+    }
+    
+    data[i] = pos;
+    counting[pos]--;
+  }
+}
+
+// overloaded method for char data
+public static void countingSort(char[] data) {
+  
+  // 128 ASCII characters
+  int[] counting = new int[128];
+  
+  for (char val: data) {
+    counting[val]++;
+  }
+  
+  // sort data
+  int pos = 0;
+  for (int i = 0; i < data.length; i++) {
+    while (pos < counting.length && counting[pos] == 0) {
+      pos++;
+    }
+    
+    data[i] = (char) pos;
+    counting[pos]--;
+  }
+}
+
+private static int getMax(int[] intData) {
+  int max = Integer.MIN_VALUE;
+  
+  for (int num: intData) {
+    if (num > max) max = num;
+  }
+  
+  return max;
+}`,
       javascript: `function countingSort(data) {
   let counts = [];
   const isNumber = typeof data[0] === 'number';
